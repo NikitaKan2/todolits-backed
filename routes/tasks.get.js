f3c41f46b9d5299d598cb37a9bdce6c020e7affc
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2022-12-07 16:34:17 +0300

    add validation for page

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -9,6 +9,7 @@ router.get(
   '/tasks/:userId',
   validate([
     query('pp').optional().isInt({ min: 5, max: 20 }).withMessage('Query pp must be min 5 and max 20'),
+    query('page').optional().isInt({ min: 1, max: 200 }).withMessage('Query page must be min 1 and max 200'),
     query('filterBy').optional().custom((value) => {
       if (!['', 'done', 'undone'].includes(value)) {
         throw new Error('Query filterBy must be empty string, "done" or "undone"');
