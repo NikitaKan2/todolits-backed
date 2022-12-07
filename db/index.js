8e65ede61d34b1827ddcb819ef795a5b0dc81f39
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2022-12-07 12:09:29 +0300

    inernarl url databse

--- db/index.js
+++ db/index.js
@@ -4,6 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
