cc530f6da883627390d521d2f96cd5b2504cde4c
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2022-12-07 15:10:06 +0300

    add origin for deploy-backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 4003;
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://backend-for-todo-app.onrender.com/'],
 };
 
 app.use(cors(corsOptions));
