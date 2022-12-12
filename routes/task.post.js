91db98f75adefe3f672c3b971d011a499984678c
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2022-12-12 13:29:00 +0300

    fix post route

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import Task from '../db/Task.js';
 
 const router = new Router();
@@ -10,7 +9,6 @@ router.post(
     try {
       const normalizeTask = await Task.create({
         name: req.body.name,
-        id: uuidv4(),
       });
 
       return res.status(200).json(normalizeTask);
