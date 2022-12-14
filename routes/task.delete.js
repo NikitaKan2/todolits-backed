684849c06fc3916ffcf3b9d552fdf00d41c5e855
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2022-12-14 16:31:06 +0300

    fix delete task rout

--- routes/task.delete.js
+++ routes/task.delete.js
@@ -1,13 +1,24 @@
 import { Router } from 'express';
+import { Op } from 'sequelize';
 import Task from '../db/Task.js';
+import authMiddleware from '../middleware/auth-middleware.js';
 
 const router = new Router();
 
 router.delete(
   '/task/:id',
+  authMiddleware,
   async (req, res) => {
     try {
-      const taskToDelete = await Task.destroy({ where: { id: req.params.id }, returning: true });
+      const taskToDelete = await Task.destroy({
+        where: {
+          [Op.and]: [
+            { name: req.body.name },
+            { userId: req.user.uuid },
+          ],
+        },
+        returning: true,
+      });
       if (!taskToDelete) {
         return res.status(404).json({ message: `Task with id: ${req.params.id} not found` });
       }
